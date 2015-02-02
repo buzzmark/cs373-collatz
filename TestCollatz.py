8d65fc5e99dd1640cd1e87fe80a48cb5d33bf537
matthew2 <matthew2@cs.utexas.edu>
2015-02-02 16:03:09 -0600

    Create more unit tests. Fixes #13

--- TestCollatz.py
+++ TestCollatz.py
@@ -24,11 +24,17 @@ class TestCollatz (TestCase) :
     # read
     # ----
 
-    def test_read (self) :
+    def test_read_1 (self) :
         s    = "1 10\n"
         i, j = collatz_read(s)
         self.assertEqual(i,  1)
         self.assertEqual(j, 10)
+        
+    def test_read_2 (self) :
+        s    = "10 1\n"
+        i, j = collatz_read(s)
+        self.assertEqual(i, 10)
+        self.assertEqual(j,  1)
 
     # ----
     # eval
@@ -49,25 +55,51 @@ class TestCollatz (TestCase) :
     def test_eval_4 (self) :
         v = collatz_eval(900, 1000)
         self.assertEqual(v, 174)
-
+        
+    def test_eval_5 (self) :
+        v = collatz_eval(1000, 900)
+        self.assertEqual(v, 174)
+        
+    def test_eval_6 (self) :
+        v = collatz_eval(1, 1)
+        self.assertEqual(v, 1)
+        
     # -----
     # print
     # -----
 
-    def test_print (self) :
+    def test_print_1 (self) :
         w = StringIO()
         collatz_print(w, 1, 10, 20)
         self.assertEqual(w.getvalue(), "1 10 20\n")
+        
+    def test_print_2 (self) :
+        w = StringIO()
+        collatz_print(w, 1, 1, 1)
+        self.assertEqual(w.getvalue(), "1 1 1\n")
+
 
     # -----
     # solve
     # -----
 
-    def test_solve (self) :
+    def test_solve_1 (self) :
         r = StringIO("1 10\n100 200\n201 210\n900 1000\n")
         w = StringIO()
         collatz_solve(r, w)
         self.assertEqual(w.getvalue(), "1 10 20\n100 200 125\n201 210 89\n900 1000 174\n")
+        
+    def test_solve_2 (self) :
+        r = StringIO("1 10\n")
+        w = StringIO()
+        collatz_solve(r, w)
+        self.assertEqual(w.getvalue(), "1 10 20\n")
+        
+    def test_solve_3 (self) :
+        r = StringIO("10 1\n")
+        w = StringIO()
+        collatz_solve(r, w)
+        self.assertEqual(w.getvalue(), "10 1 20\n")
 
 # ----
 # main
